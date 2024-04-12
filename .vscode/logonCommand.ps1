3e43f7b04045a207c03446379c2f5a26ba00d00b
Michiel Dekker <dekker.michiel@outlook.com>
2024-04-12 14:10:08 +0200

    changes

--- .vscode/logonCommand.ps1
+++ .vscode/logonCommand.ps1
@@ -1,5 +1,5 @@
 Write-host "Testing has started..." -ForegroundColor Cyan
-Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\devops\Deploy-Application.exe" -Wait
+Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\7-Zip\Deploy-Application.exe" -Wait
 Write-host "Installation completed" -ForegroundColor DarkGreen
 Write-host "you have 60 seconds to verify the installation before it is automatically uninstalled" -ForegroundColor Cyan
 
@@ -11,7 +11,7 @@ while (($TimeRemaining = ($EndTime - [datetime]::UtcNow)) -gt 0) {
   Start-Sleep 1
 }
 
-Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\devops\Deploy-Application.exe" -ArgumentList "Uninstall" -Wait
+Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\7-Zip\Deploy-Application.exe" -ArgumentList "Uninstall" -Wait
 Write-host "test completed" -ForegroundColor DarkGreen
 Write-host "You can close sandbox now!" -ForegroundColor Cyan
 Read-Host -Prompt "Press any key to continue..."
