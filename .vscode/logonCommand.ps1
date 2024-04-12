b236c544baf289da5cf89d62f2a5a9eb4e53e39e
Michiel Dekker <dekker.michiel@outlook.com>
2024-04-12 14:59:41 +0200

    changes

--- .vscode/logonCommand.ps1
+++ .vscode/logonCommand.ps1
@@ -1,5 +1,8 @@
+# TODO Add application name
+$Application = "7-Zip"
+
 Write-host "Testing has started..." -ForegroundColor Cyan
-Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\7-Zip\Deploy-Application.exe" -Wait
+Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\$Application\Deploy-Application.exe" -Wait
 Write-host "Installation completed" -ForegroundColor DarkGreen
 Write-host "you have 60 seconds to verify the installation before it is automatically uninstalled" -ForegroundColor Cyan
 
@@ -11,7 +14,7 @@ while (($TimeRemaining = ($EndTime - [datetime]::UtcNow)) -gt 0) {
   Start-Sleep 1
 }
 
-Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\7-Zip\Deploy-Application.exe" -ArgumentList "Uninstall" -Wait
+Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Desktop\$Application\Deploy-Application.exe" -ArgumentList "Uninstall" -Wait
 Write-host "test completed" -ForegroundColor DarkGreen
 Write-host "You can close sandbox now!" -ForegroundColor Cyan
 Read-Host -Prompt "Press any key to continue..."
