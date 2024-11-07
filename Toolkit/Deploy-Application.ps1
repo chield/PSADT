b1b7522c2088224418e5afd27a7ad1a31eb25a2b
Michiel Dekker <dekker.michiel@outlook.com>
2024-11-07 14:55:48 +0100

    completed

--- Toolkit/Deploy-Application.ps1
+++ Toolkit/Deploy-Application.ps1
@@ -106,14 +106,14 @@ Try {
     ##* VARIABLE DECLARATION
     ##*===============================================
     ## TODO Variables: Application
-    [String]$appVendor = ''
-    [String]$appName = ''
-    [String]$appVersion = ''
+    [String]$appVendor = 'Audiocodes'
+    [String]$appName = 'INI viewer'
+    [String]$appVersion = '2.11'
     [String]$appArch = ''
     [String]$appLang = 'EN'
     [String]$appRevision = '01'
     [String]$appScriptVersion = '1.0.0'
-    [String]$appScriptDate = 'xx/xx/2024'
+    [String]$appScriptDate = '07/11/2024'
     [String]$appScriptAuthor = 'Michiel Dekker'
     ##*===============================================
     ## TODO Variables: Install Titles (Only set here to override defaults set by the toolkit)
@@ -206,7 +206,8 @@ Try {
 
         ## TODO <Perform Installation tasks here>
 
-
+        Execute-process -Path "$dirfiles\iniedit-setup.exe" -Parameters '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART' -WindowStyle 'Hidden'
+        
         ##*===============================================
         ##* POST-INSTALLATION
         ##*===============================================
@@ -250,6 +251,7 @@ Try {
 
         ## TODO <Perform Uninstallation tasks here>
 
+        Execute-Process -Path "C:\Program Files\iniedit\unins000.exe"
 
         ##*===============================================
         ##* POST-UNINSTALLATION
